b2e61eaf9bb6dd5d6cc20537bdf3b3b60706cfa1
awais786327 <awais3227443@gmail.com>
2018-01-10 17:39:59 +0500

    init

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name radUlFasaadApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the radUlFasaadApp
+ */
+angular.module('radUlFasaadApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name radUlFasaadApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the radUlFasaadApp
+ */
+angular.module('radUlFasaadApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
