9e6b6d36b1ca30fe37a3fd0b9b9d60c528be47cb
awais786327 <awais3227443@gmail.com>
2018-01-31 14:12:09 +0500

    reformated code, implemented login signup design

--- web/app/scripts/controllers/main.js
+++ web/app/scripts/controllers/main.js
@@ -5,17 +5,85 @@
     .module('radUlFasaadApp')
     .controller('MainCtrl', MainCtrl);
 
-  MainCtrl.$inject = [];
+  MainCtrl.$inject = ['$timeout'];
 
-  function MainCtrl() {
+  function MainCtrl($timeout) {
     /* jshint validthis: true */
     var vm = this;
+
 
     vm.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+
+    vm.openLoginModal = openLoginModal;
+    vm.openRegisterModal = openRegisterModal;
+    vm.showLoginForm = showLoginForm;
+    vm.showRegisterForm = showRegisterForm;
+    vm.loginAjax = loginAjax;
+
+
+    function openLoginModal(){
+      showLoginForm();
+      $timeout(function(){
+        $('#loginModal').modal('show');
+      }, 230);
+
+    }
+    function openRegisterModal(){
+      showRegisterForm();
+      $timeout(function(){
+        $('#loginModal').modal('show');
+      }, 230);
+
+    }
+    function showLoginForm(){
+      $('#loginModal .registerBox').fadeOut('fast',function(){
+        $('.loginBox').fadeIn('fast');
+        $('.register-footer').fadeOut('fast',function(){
+          $('.login-footer').fadeIn('fast');
+        });
+
+        $('.modal-title').html('Login with');
+      });
+      $('.error').removeClass('alert alert-danger').html('');
+    }
+    function showRegisterForm(){
+      $('.loginBox').fadeOut('fast',function(){
+        $('.registerBox').fadeIn('fast');
+        $('.login-footer').fadeOut('fast',function(){
+          $('.register-footer').fadeIn('fast');
+        });
+        $('.modal-title').html('Register with');
+      });
+      $('.error').removeClass('alert alert-danger').html('');
+
+    }
+    function loginAjax(){
+      /*   Remove this comments when moving to server
+       $.post( "/login", function( data ) {
+       if(data == 1){
+       window.location.replace("/home");
+       } else {
+       shakeModal();
+       }
+       });
+       */
+
+      /*   Simulate error message from the server   */
+      shakeModal();
+    }
+    function shakeModal(){
+      $('#loginModal .modal-dialog').addClass('shake');
+      $('.error').addClass('alert alert-danger').html("Invalid email/password combination");
+      $('input[type="password"]').val('');
+      $timeout( function(){
+        $('#loginModal .modal-dialog').removeClass('shake');
+      }, 1000 );
+    }
   }
 
 })();
