1340dddb09104fb16073c5c38b93339cdd6a81b8
Syed_Bakhtiyar <syedbakhtiyar120@gmail.com>
2018-02-13 13:21:18 +0500

    Speec recognization Sample is ready

--- android/RaddulFasad/app/src/main/java/com/example/ikodepc_1/raddulfasad/LoginActivity.java
+++ android/RaddulFasad/app/src/main/java/com/example/ikodepc_1/raddulfasad/LoginActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,8 +14,20 @@ import android.widget.TextView;
 import com.example.ikodepc_1.raddulfasad.interfaces.CommonMethods;
 import com.example.ikodepc_1.raddulfasad.utils.Util;
 
+import org.json.JSONObject;
+
+import javax.net.ssl.SSLContext;
+
+import cz.msebera.android.httpclient.client.methods.HttpPost;
+import cz.msebera.android.httpclient.conn.scheme.Scheme;
+import cz.msebera.android.httpclient.conn.ssl.SSLSocketFactory;
+import cz.msebera.android.httpclient.entity.StringEntity;
+import cz.msebera.android.httpclient.impl.client.BasicResponseHandler;
+import cz.msebera.android.httpclient.impl.client.DefaultHttpClient;
+
 public class LoginActivity extends AppCompatActivity implements CommonMethods,View.OnClickListener {
 
+    private static final String URL_LOGIN = "https://us-central1-rad-ul-fasaad.cloudfunctions.net/login";
     EditText edt_email, edt_pswd;
     Button login_button;
     TextView signupLink;
@@ -74,13 +87,100 @@ public class LoginActivity extends AppCompatActivity implements CommonMethods,Vi
 
         @Override
         protected String doInBackground(String... strings) {
-            return null;
+            String response = null;
+            try {
+                SSLSocketFactory sf = new SSLSocketFactory(
+                        SSLContext.getInstance("Default"),
+                        SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+                Scheme sch = new Scheme("https", 443, sf);
+                String object = registration(strings[0],strings[1]);
+                HttpPost httpPost = new HttpPost(URL_LOGIN);
+                StringEntity stringEntity = new StringEntity(object);
+                httpPost.setEntity(stringEntity);
+                httpPost.setHeader("Content-type","application/json");
+                DefaultHttpClient defaultHttpClient = new DefaultHttpClient();
+                defaultHttpClient.getConnectionManager().getSchemeRegistry().register(sch);
+                BasicResponseHandler basicResponseHandler = new BasicResponseHandler();
+                response = defaultHttpClient.execute(httpPost,basicResponseHandler);
+            } catch (Exception e) {
+                Log.d("Exception", e.toString());
+            }
+            return response;
         }
 
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
+            if(s != null){
+                Log.d("Exception", "onPostExecute: "+s);
+                startActivity(new Intent(LoginActivity.this,MainActivity.class));
+            }
             this.progressDialog.dismiss();
         }
     }
+
+    private String registration(String email, String password){
+        String json_body = null;
+        try {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("email",email);
+            jsonObject.put("password",password);
+            json_body = jsonObject.toString(1);
+        }catch (Exception e){
+            Log.d("Exception", "registration: "+e);
+        }
+        return json_body;
+    }
 }
+
+//    functions urls:
+//
+//
+//        Function URL (signup): https://us-central1-rad-ul-fasaad.cloudfunctions.net/signup
+//        + functions[login]: Successful update operation.
+//        Function URL (login): https://us-central1-rad-ul-fasaad.cloudfunctions.net/login
+//        + functions[webhook]: Successful create operation.
+//        Function URL (webhook): https://us-central1-rad-ul-fasaad.cloudfunctions.net/webhook
+//        + functions[talk]: Successful create operation.
+//        Function URL (talk): https://us-central1-rad-ul-fasaad.cloudfunctions.net/talk
+
+
+////////////////////
+
+/*
+
+
+login signup getprofile and talk apis are ready:
+talk api will be used to to talk with chatbot
+interfaces of api are following
+are request are post
+
+
+
+interface signup {
+ firstName: string
+ lastName: string
+ phoneNumber: number
+ email: string
+ password: string
+}
+interface login {
+ email: string
+ password: string
+}
+interface profile {
+ uid: string
+ token: string
+}
+
+
+
+
+interface talk {
+ platform: string, // 0 for web, 1 for android
+ text: string,
+ uid: string,
+ token: string
+}
+
+ */
--- android/RaddulFasad/app/src/main/java/com/example/ikodepc_1/raddulfasad/MainActivity.java
+++ android/RaddulFasad/app/src/main/java/com/example/ikodepc_1/raddulfasad/MainActivity.java
@@ -1,15 +1,168 @@
 package com.example.ikodepc_1.raddulfasad;
 
+import android.content.Intent;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Switch;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements RecognitionListener {
+
+    private EditText returnedText;
+    private Switch toggleButton;
+    private ProgressBar progressBar;
+    private SpeechRecognizer speech = null;
+    private Intent recognizerIntent;
+    private String LOG_TAG = "VoiceRecognitionActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        speech = SpeechRecognizer.createSpeechRecognizer(this);
+        speech.setRecognitionListener(this);
+        recognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE,
+                "en");
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,
+                this.getPackageName());
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_WEB_SEARCH);
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS, true);
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_SPEECH_INPUT_MINIMUM_LENGTH_MILLIS, 20000);
+        returnedText = (EditText) findViewById(R.id.returnedText);
+        toggleButton = (Switch) findViewById(R.id.switch_button);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar1);
+        progressBar.setVisibility(View.INVISIBLE);
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    progressBar.setVisibility(View.VISIBLE);
+                    progressBar.setIndeterminate(true);
+                    speech.startListening(recognizerIntent);
+                } else {
+                    progressBar.setIndeterminate(false);
+                    progressBar.setVisibility(View.INVISIBLE);
+                    speech.stopListening();
+                }
+            }
+        });
     }
+
+    @Override
+    public void onBeginningOfSpeech() {
+        Log.i(LOG_TAG, "onBeginningOfSpeech");
+        progressBar.setIndeterminate(false);
+        progressBar.setMax(10);
+    }
+
+    @Override
+    public void onBufferReceived(byte[] buffer) {
+        Log.i(LOG_TAG, "onBufferReceived: " + buffer);
+    }
+
+    @Override
+    public void onEndOfSpeech() {
+        Log.i(LOG_TAG, "onEndOfSpeech");
+        progressBar.setIndeterminate(true);
+        toggleButton.setChecked(false);
+    }
+
+    @Override
+    public void onError(int errorCode) {
+        String errorMessage = getErrorText(errorCode);
+        Log.d(LOG_TAG, "FAILED " + errorMessage);
+        returnedText.setText(errorMessage);
+        toggleButton.setChecked(false);
+    }
+
+    @Override
+    public void onEvent(int arg0, Bundle arg1) {
+        Log.i(LOG_TAG, "onEvent");
+    }
+
+    @Override
+    public void onPartialResults(Bundle arg0) {
+        Log.i(LOG_TAG, "onPartialResults");
+        ArrayList<String> matches = arg0
+                .getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+        String text = "";
+        for (String result : matches)
+            text += result + "\n";
+
+        returnedText.setText(text);
+
+
+    }
+
+    @Override
+    public void onReadyForSpeech(Bundle arg0) {
+        Log.i(LOG_TAG, "onReadyForSpeech");
+    }
+
+    @Override
+    public void onResults(Bundle results) {
+        Log.i(LOG_TAG, "onResults");
+
+    }
+
+    @Override
+    public void onRmsChanged(float rmsdB) {
+        Log.i(LOG_TAG, "onRmsChanged: " + rmsdB);
+        progressBar.setProgress((int) rmsdB);
+
+    }
+
+    public static String getErrorText(int errorCode) {
+        String message;
+        switch (errorCode) {
+            case SpeechRecognizer.ERROR_AUDIO:
+                message = "Audio recording error";
+                break;
+            case SpeechRecognizer.ERROR_CLIENT:
+                message = "Client side error";
+                break;
+            case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
+                message = "Insufficient permissions";
+                break;
+            case SpeechRecognizer.ERROR_NETWORK:
+                message = "Network error";
+                break;
+            case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
+                message = "Network timeout";
+                break;
+            case SpeechRecognizer.ERROR_NO_MATCH:
+                message = "No match";
+                break;
+            case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
+                message = "RecognitionService busy";
+                break;
+            case SpeechRecognizer.ERROR_SERVER:
+                message = "error from server";
+                break;
+            case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
+                message = "No speech input";
+                break;
+            default:
+                message = "Didn't understand, please try again.";
+                break;
+        }
+        return message;
+    }
+
+
+
 }
 
 //
