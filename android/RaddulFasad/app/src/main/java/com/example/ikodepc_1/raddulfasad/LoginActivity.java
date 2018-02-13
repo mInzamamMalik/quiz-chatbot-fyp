5e71088671c3d2bc00ce560c4b3f14563e5ea700
Syed_Bakhtiyar <syedbakhtiyar120@gmail.com>
2018-02-13 12:47:52 +0500

    Include Email Validation And Custom Material Toast

--- android/RaddulFasad/app/src/main/java/com/example/ikodepc_1/raddulfasad/LoginActivity.java
+++ android/RaddulFasad/app/src/main/java/com/example/ikodepc_1/raddulfasad/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.example.ikodepc_1.raddulfasad;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +11,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.ikodepc_1.raddulfasad.interfaces.CommonMethods;
+import com.example.ikodepc_1.raddulfasad.utils.Util;
 
 public class LoginActivity extends AppCompatActivity implements CommonMethods,View.OnClickListener {
 
@@ -43,10 +46,41 @@ public class LoginActivity extends AppCompatActivity implements CommonMethods,Vi
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.login:
+                if(Util.etValidate(edt_email) && Util.isValidEmail(edt_email) && Util.etValidate(edt_pswd)){
+                    new Login().execute(edt_email.getText().toString().trim(),edt_pswd.getText().toString().trim());
+                }
                 break;
             case R.id.signup_link:
                 startActivity(new Intent(LoginActivity.this,SignUpActivity.class));
                 break;
+        }
+    }
+
+    private class Login extends AsyncTask<String,Void,String>{
+
+        ProgressDialog progressDialog;
+
+        public Login() {
+            this.progressDialog = new ProgressDialog(LoginActivity.this);
+            this.progressDialog.setMessage("Please Wait...");
+            this.progressDialog.setCancelable(false);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            this.progressDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            this.progressDialog.dismiss();
         }
     }
 }
