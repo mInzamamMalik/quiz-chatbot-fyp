1cf2c5ac267216c13b4360cc6f97a3bf3cde68e2
awais786327 <awais3227443@gmail.com>
2018-02-22 16:54:38 +0500

    added aws sdk and a route component for request

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var translate = require('./routes/translate');
 
 var app = express();
 
@@ -27,7 +27,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/translate', translate);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- server/routes/translate.js
+++ server/routes/translate.js
@@ -3,6 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  console.info('req body ', req.body);
+  console.info('req headers ', req.headers);
+  console.info('req query ', req.query);
+  console.info('req params ', req.params);
   res.send('respond with a resource');
 });
 
