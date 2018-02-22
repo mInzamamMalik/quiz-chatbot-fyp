7af10dc310d716168e4cbb440b1fe838f17901d7
awais786327 <awais3227443@gmail.com>
2018-02-22 14:24:25 +0500

    convert SSML to speech work, added gitignore  files for server

--- web/app/scripts/controllers/main.js
+++ web/app/scripts/controllers/main.js
@@ -28,11 +28,51 @@
     vm.showRegisterForm = showRegisterForm;
     vm.loginAjax = loginAjax;
     vm.logout = logout;
-
     vm.signInWithGoogle = signInWithGoogle;
-
     vm.registerUser = registerUser;
 
+
+    init();
+
+
+    function init() {
+      AWS.config.region = 'xxx';
+      AWS.config.accessKeyId = 'xxxxxxxxx';
+      AWS.config.secretAccessKey = 'xxxxxxxxxxxxxxxxxxxxxx';
+
+      var polly = new AWS.Polly();
+      var params = {
+        OutputFormat: 'mp3', /* required */
+        Text: ` <speak>
+                   hisaaaa ssd<break time="1s"/>
+                   In the middle of the 10/3/2014 <sub alias="World Wide Web Consortium">W3C</sub> meeting
+                   he shouted, "Score!" quite loudly. When his boss stared at him, he repeated
+                   <amazon:effect name="whispered">"Score"</amazon:effect> in a whisper.
+                </speak>`, /* required */
+        VoiceId: 'Joanna', /* required */
+        SampleRate: '22050',
+        TextType: 'ssml'
+      };
+
+      polly.synthesizeSpeech(params, function(err, data) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('stream ', data);
+
+          var blob = new Blob([data.AudioStream], {type : data.ContentType});
+
+          $timeout(function() {
+            var reader = new FileReader();
+            reader.onload = function(e) {
+              angular.element("#say_speak").attr("src", e.target.result);
+            };
+            reader.readAsDataURL(blob);
+          });
+
+        }
+      });
+    }
 
     function openLoginModal(){
       showLoginForm();
